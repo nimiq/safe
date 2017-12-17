2724e245c34e432b7f0d485b05c38dba340a3fd8
robinlinus <robinlinus@users.noreply.github.com>
2017-12-16 22:05:20 -0600

    Router should handle paths in url fragment

--- src/x-app.js
+++ src/x-app.js
@@ -1,32 +1,30 @@
 class XApp extends XElement {
 
-    get __tagName() {
-        return 'body';
-    }
+    get __tagName() { return 'body' }
+
+    get defaultLocation() { return '#home' }
 
     onCreate() {
-        window.addEventListener('popstate', () => this._setState());
-        this._setState();
+        window.addEventListener('popstate', () => this._parseUriFragment());
+        this._parseUriFragment();
     }
 
-    _setState() {
-        const state = location.hash.substr(1);
-        if (state === '') {
-            location = '#home';
-            return;
-        }
+    _parseUriFragment() {
+        const fragment = decodeURIComponent(location.hash.substr(1))
+        const path = fragment.split('/');
+        const state = path[0];
+        if (state === '') return location = this.defaultLocation();
+        this._stateChanged(state, path);
+    }
+
+    _stateChanged(state, path) {
         document.body.className = 'state-' + state;
-        this._showView(state);
-    }
-
-    _showView(state) {
         const stateCased = state[0].toUpperCase() + state.substring(1);
         const viewName = '$view' + stateCased;
-        if (this[viewName] instanceof XElement) {
-            document.activeElement.blur();
-            if (this.$currView) this.$currView.onHide();
-            this.$currView = this[viewName];
-            this.$currView.onShow();
-        }
+        if (!(this[viewName] instanceof XElement)) return;
+        document.activeElement.blur();
+        if (this.$currView) this.$currView.onHide();
+        this.$currView = this[viewName];
+        this.$currView.onShow();
     }
 }
