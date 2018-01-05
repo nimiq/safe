a3ac19a8877f86b30523d31f86baf22d7220db62
Sören Schwert <hello@soerenschwert.de>
2018-01-05 17:52:18 +0100

    Clear existing words on update, avoid innerHTML, select text on click
    Former-commit-id: 63652f448fa1ac5a45bc73585228641eb2a5982b

--- x-mnemonic-phrase/x-mnemonic-phrase.js
+++ x-mnemonic-phrase/x-mnemonic-phrase.js
@@ -1,16 +1,18 @@
 class XMnemonicPhrase extends XElement {
-
-    onCreate() {
-        // this.privateKey = 'a18532abfb31ba4e26d64a3ac3430969639aeb5f84b1c4124da0f3e323cdaced';
-    }
-
     set privateKey(privateKey) {
         const phrase = MnemonicPhrase.keyToMnemonic(privateKey);
         const words = phrase.split(' ');
+
+        // Clear existing words
+        while(this.$el.firstChild) this.$el.removeChild(this.$el.firstChild);
+
         words.forEach(word => {
-            const $el = document.createElement('span');
-            $el.innerHTML = word + '<i>&nbsp;</i>';
-            this.$el.appendChild($el);
+            const $span = document.createElement('span');
+            $span.textContent = word;
+            const $i = document.createElement('i');
+            $i.textContent = String.fromCharCode(160); // non-breakable space
+            $span.appendChild($i);
+            this.$el.appendChild($span);
         });
     }
 }
