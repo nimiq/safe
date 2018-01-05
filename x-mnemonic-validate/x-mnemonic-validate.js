a29aedc83358bd1843d4ea8a933fa8ab83b8bc57
Sören Schwert <hello@soerenschwert.de>
2018-01-06 00:10:46 +0100

    Start using x-slider for x-mnemonic-validate
    Former-commit-id: b18b36f58ff113e70b9cd9670b3fe5376e5ab268

--- x-mnemonic-validate/x-mnemonic-validate.js
+++ x-mnemonic-validate/x-mnemonic-validate.js
@@ -1,12 +1,11 @@
 class XMnemonicValidate extends XElement {
     onCreate() {
+        this.$slider = new XSlider();
+
         this.$indices = this.$el.querySelectorAll('x-mnemonic-validate-index');
         this.$words = this.$el.querySelectorAll('[x-mnemonic-validate-word]');
-        this.$button = this.$('button');
 
         this.updateRequiredWords();
-
-        this.$button.addEventListener('click', this.validate.bind(this));
     }
 
     _generateIndices() {
@@ -39,20 +38,11 @@ class XMnemonicValidate extends XElement {
         for(let i = 0; i < this.indices.length; i++) {
             if(this.$words[i].value !== words[this.indices[i]]) {
                 this.fire('invalid');
-                this._shakeButton();
                 return false;
             }
         }
 
         this.fire('validated');
-        this.$button.setAttribute('disabled', 'disabled');
-        this.$button.textContent = 'Validated';
-
         return true;
-    }
-
-    _shakeButton() {
-        this.$button.classList.add('shake');
-        setTimeout(function() { this.$button.classList.remove('shake'); }.bind(this), 820);
     }
 }
