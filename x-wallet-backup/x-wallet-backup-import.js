7345d8005b217b2f15b9a107198664a76a16e9b9
robinlinus <robinlinus@users.noreply.github.com>
2018-01-05 21:45:57 +0100

    Add wallet-backup-import
    Former-commit-id: 908d6b8d04a8234b88550d9332c666b4350882eb

---
+++ x-wallet-backup/x-wallet-backup-import.js
@@ -0,0 +1,38 @@
+class XWalletBackupImport extends XElement {
+    onCreate() {
+        this._bindHandlers();
+    }
+
+    _bindHandlers() {
+        const dropZone = document.body;
+        dropZone.addEventListener('drop', e => this._onFileDrop(e), false);
+        dropZone.addEventListener('dragover', e => this._onDragOver(e), false);
+
+        dropZone.addEventListener('dragexit', e => this._onDragEnd(e), false);
+        dropZone.addEventListener('dragend', e => this._onDragEnd(e), false);
+    }
+
+    _onFileDrop(event) {
+        this._stopPropagation(event);
+        this._onDragEnd();
+        // Get files
+        const files = event.dataTransfer.files;
+        const file = files[0];
+        this.fire('x-wallet-import', file);
+    }
+
+    _onDragOver(event) {
+        this._stopPropagation(event);
+        event.dataTransfer.dropEffect = 'copy';
+        this.$el.setAttribute('active', 1);
+    }
+
+    _stopPropagation(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    _onDragEnd() {
+        this.$el.removeAttribute('active');
+    }
+}
