c60e27d24216d5ce2309941b856828bab77bf95b
robinlinus <robinlinus@users.noreply.github.com>
2018-01-05 15:45:46 +0100

    Rename private-data to mnemonic-phrase, first impl
    Former-commit-id: d0d7740417e26618783dffccbea16325c3ffe0df

--- private-data/private-data.js
+++
@@ -1,3 +0,0 @@
-class PrivateData extends XElement{
-	
-}
---
+++ x-mnemonic-phrase/x-mnemonic-phrase.js
@@ -0,0 +1,16 @@
+class XMnemonicPhrase extends XElement {
+
+    onCreate() {
+        this.privateKey = 'a18532abfb31ba4e26d64a3ac3430969639aeb5f84b1c4124da0f3e323cdaced';
+    }
+
+    set privateKey(privateKey) {
+        const phrase = MnemonicPhrase.keyToMnemonic(privateKey);
+        const words = phrase.split(' ');
+        words.forEach(word => {
+            const $el = document.createElement('span');
+            $el.textContent = word;
+            this.$el.appendChild($el);
+        });
+    }
+}
