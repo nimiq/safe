89dbd9b1e345ccc402e0945c3338d67c7925920a
robinlinus <robinlinus@users.noreply.github.com>
2018-01-06 12:45:18 +0100

    Inline html for numpad
    Former-commit-id: 60d2ac7d6d450372da76218f3829cb672fd8e1b2

--- x-numpad/x-numpad.js
+++ x-numpad/x-numpad.js
@@ -55,6 +55,22 @@ class Numpad extends XElement {
         this._value = value;
         this.fire('x-change', value);
     }
+
+    html(){
+        return `
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>.</button>
+            <button>0</button>
+            <button>&lt;</button>`
+    }
 }
 
 // Todo: Fix bugs when entering 0.123 and deleting
