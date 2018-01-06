40e1aa864edf4fac9258070916e9f174f6c5e426
robinlinus <robinlinus@users.noreply.github.com>
2018-01-06 23:08:57 +0100

    Refactor x-mnemonic-input
    Former-commit-id: 5ec0b0ebf82298fcfa38de628b9e1b7cdecddcf2

--- x-mnemonic-input/x-mnemonic-input.js
+++ x-mnemonic-input/x-mnemonic-input.js
@@ -3,8 +3,8 @@ class XMnemonicInput extends XElement {
     onCreate() {
         this.$fields = [];
         this.$form = document.createElement('form');
-        for(let i = 0; i < 24; i++) {
-            const field = new XMnemonicInputField(document.createElement('x-word-input'));
+        for (let i = 0; i < 24; i++) {
+            const field = XMnemonicInputField.createElement();
             this.$form.appendChild(field.$el);
             this.$fields.push(field);
         }
@@ -16,10 +16,10 @@ class XMnemonicInput extends XElement {
     }
 
     _onFieldComplete(e) {
-        if(e.detail[0].$input === document.activeElement && !e.detail[1]) {
+        if (e.detail[0].$input === document.activeElement && !e.detail[1]) {
             // Find active field
             const index = Array.prototype.indexOf.call(this.$fields, e.detail[0]);
-            if(index < this.$fields.length - 1)
+            if (index < this.$fields.length - 1)
                 // Set focus to next field
                 this.$fields[index + 1].$input.focus();
         }
@@ -29,19 +29,16 @@ class XMnemonicInput extends XElement {
 
     _checkPhraseComplete() {
         const check = this.$fields.find(field => !field._complete);
-        if(typeof check === 'undefined') {
-            const mnemonic = this.$fields.map(field => field.$input.value).join(' ');
-            this.fire('recovered', MnemonicPhrase.mnemonicToKey(mnemonic));
-        }
+        if (typeof check !== 'undefined') return;
+        const mnemonic = this.$fields.map(field => field.$input.value).join(' ');
+        const privateKey = MnemonicPhrase.mnemonicToKey(mnemonic);
+        this.fire('recovered', privateKey);
     }
 }
 
 class XMnemonicInputField extends XElement {
     onCreate() {
-        this.$input = document.createElement('input');
-        this.$input.type = 'text';
-        this.$el.appendChild(this.$input);
-
+        this.$input = this.$('input');
         this.$input.addEventListener('keydown', e => this._onKeyDown(e));
         this.$input.addEventListener('input', e => this._onInput(e));
         this.$input.addEventListener('blur', e => this._onBlur(e));
@@ -51,12 +48,12 @@ class XMnemonicInputField extends XElement {
 
     _onKeyDown(e) {
         const value = this.$input.value;
-        if(e.keyCode === 32 /* space */ || e.keyCode === 9 /* tab */ || e.type === 'blur') {
-            if(value.length >= 3) {
+        if (e.keyCode === 32 /* space */ || e.keyCode === 9 /* tab */ || e.type === 'blur') {
+            if (value.length >= 3) {
                 const index = MnemonicPhrase.DEFAULT_WORDLIST.indexOf(value);
-                if(index > -1) {
+                if (index > -1) {
                     this._complete = true;
-                    this.fire('complete', [this, e.keyCode === 9 /* tab */]);
+                    this.fire('complete', [this, e.keyCode === 9 /* tab */ ]);
                     this.$input.classList.add('complete');
                 }
                 // else {
@@ -65,22 +62,24 @@ class XMnemonicInputField extends XElement {
                 // }
             }
 
-            if(e.keyCode === 32 /* space */) e.preventDefault();
+            if (e.keyCode === 32 /* space */ ) e.preventDefault();
         }
     }
 
     _onInput(e) {
         const value = this.$input.value;
-        if(this._value !== value) {
-            this._complete = false;
-            this.$input.classList.remove('complete');
-        }
+        if (this._value === value) return;
+        this._complete = false;
+        this.$input.classList.remove('complete');
         this._value = value;
     }
 
     _onBlur(e) {
-        if(this._complete) return;
-
+        if (this._complete) return;
         this._onKeyDown(e);
+    }
+
+    html() {
+        return `<input type="text" autocomplete="off">`
     }
 }
