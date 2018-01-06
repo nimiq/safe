8fe481956617fc1743cea835a308ddcb3d2e55f1
robinlinus <robinlinus@users.noreply.github.com>
2018-01-06 14:24:33 +0100

    Remove code to select/copy mnemonic phrase
    Former-commit-id: 86b52c1501b2b63b9c814df2265b9796e87e03b9

--- x-mnemonic-phrase/x-mnemonic-phrase.js
+++ x-mnemonic-phrase/x-mnemonic-phrase.js
@@ -4,14 +4,11 @@ class XMnemonicPhrase extends XElement {
         const words = phrase.split(/\s+/g);
 
         // Clear existing words
-        while(this.$el.firstChild) this.$el.removeChild(this.$el.firstChild);
+        this.clear();
 
         words.forEach(word => {
             const $span = document.createElement('span');
             $span.textContent = word;
-            const $i = document.createElement('i');
-            $i.textContent = String.fromCharCode(160); // non-breakable space
-            $span.appendChild($i);
             this.$el.appendChild($span);
         });
     }
