29a9c1f5ead8e5b745e22d31f1918dd842e34f87
robinlinus <robinlinus@users.noreply.github.com>
2018-01-06 13:23:12 +0100

    Add XMnemonicValidateSlide
    Former-commit-id: 55a7cf053151ed6bc3050c8a52d89a5f92672fd2

--- x-mnemonic-validate/x-mnemonic-validate.js
+++ x-mnemonic-validate/x-mnemonic-validate.js
@@ -4,6 +4,13 @@ class XMnemonicValidate extends XElement {
         this.addEventListener('click', e => this._onClick(e));
 
         // this.reset();
+
+
+        this.$slide1 = new XMnemonicValidateSlide(this.$('#slide1'));
+        this.$slide2 = new XMnemonicValidateSlide(this.$('#slide2'));
+        this.$slide3 = new XMnemonicValidateSlide(this.$('#slide3'));
+
+        this.addEventListener('x-mnemonic-word', e => this._onWordSelected(e))
     }
 
     set privateKey(privateKey) {
@@ -40,7 +47,7 @@ class XMnemonicValidate extends XElement {
         words[this._mnemonic[wordIndex]] = wordIndex;
 
         // Select 8 unique words from the mnemonic phrase
-        while(Object.keys(words).length < 8) {
+        while (Object.keys(words).length < 8) {
             const index = Math.floor(Math.random() * 24);
             words[this._mnemonic[index]] = index;
         }
@@ -54,7 +61,7 @@ class XMnemonicValidate extends XElement {
     }
 
     _showActiveSlide(instantly) {
-        if(instantly) this.$slider.jumpTo(this._activeSlide);
+        if (instantly) this.$slider.jumpTo(this._activeSlide);
         else this.$slider.slideTo(this._activeSlide);
     }
 
@@ -65,15 +72,15 @@ class XMnemonicValidate extends XElement {
     }
 
     _isValidWord(wordIndex, word) {
-        if(!this._mnemonic) throw new Error('Mnemonic to validate against is not set');
+        if (!this._mnemonic) throw new Error('Mnemonic to validate against is not set');
 
-        if(word !== this._mnemonic[wordIndex]) return false;
+        if (word !== this._mnemonic[wordIndex]) return false;
 
         return true;
     }
 
     _onButtonPressed($button) {
-        if(!this._isValidWord(this.requiredWords[this._activeSlide], $button.textContent)) {
+        if (!this._isValidWord(this.requiredWords[this._activeSlide], $button.textContent)) {
             this._shake($button);
             setTimeout(() => this.reset(), 1000);
             return;
@@ -81,7 +88,7 @@ class XMnemonicValidate extends XElement {
 
         this._correct($button);
 
-        if(this._activeSlide === 2) this.fire('validated');
+        if (this._activeSlide === 2) this.fire('validated');
         else setTimeout(() => this._next(), 500);
     }
 
@@ -102,5 +109,52 @@ class XMnemonicValidate extends XElement {
 
     _correct($el) {
         $el.classList.add('correct');
+    }
+}
+
+
+
+
+
+class XMnemonicValidateSlide extends XElement {
+
+    onCreate() {
+        this.$words = this.$$('x-word');
+        this.$targetIndex = this.$('x-target-index');
+        this.addEventListener('click', e => this._onClick(e))
+    }
+
+    set wordlist(wordlist) {
+        wordlist.forEach((word, index) => this.$words[index].textContent = word);
+    }
+
+    set targetIndex(index) {
+        this.$targetIndex.textContent = index;
+    }
+
+    _onClick(e) {
+        if (e.target.localName !== 'button') return;
+        const word = e.target.textContent;
+        this.fire('x-mnemonic-word', {
+            slide: this,
+            word: word
+        });
+    }
+
+    html() {
+        return `
+            <p>Please select the following word from your phrase:</p>
+            <x-target-index></x-target-index>
+            <x-wordlist>
+                <x-word></x-word>
+                <x-word></x-word>
+                <x-word></x-word>
+                <x-word></x-word>
+                <x-word></x-word>
+                <x-word></x-word>
+                <x-word></x-word>
+                <x-word></x-word>
+            </x-wordlist>
+        `
     }
 }
