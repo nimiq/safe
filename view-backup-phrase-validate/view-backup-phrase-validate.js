3922c0db0cecd1192128c3778424eb63e055b860
robinlinus <robinlinus@users.noreply.github.com>
2018-01-11 20:32:10 -0600

    Add view-backup-phrase-validate
    Former-commit-id: 54e249eb3c5848aba4ed64acf7e3379be5f1dee6

---
+++ view-backup-phrase-validate/view-backup-phrase-validate.js
@@ -0,0 +1,13 @@
+class ViewBackupPhraseValidate extends XElement {
+    children() { return [XMnemonicValidate] }
+    html() {
+        return `
+			<h1>Validate Recovery Phrase</h1>
+			<x-mnemonic-validate></x-mnemonic-validate>
+		`
+    }
+
+    set privateKey(privateKey) {
+        this.$mnemonicValidate.privateKey = privateKey
+    }
+}
