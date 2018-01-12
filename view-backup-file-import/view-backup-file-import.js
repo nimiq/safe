8ad67bf8c1a103befca5744b83538ca5d17817b5
robinlinus <robinlinus@users.noreply.github.com>
2018-01-12 11:31:17 -0600

    Add view-backup-file-import demo
    Former-commit-id: cc7a9a2e7e0335fecdc6df8da96217ca9ad6b111

--- view-backup-file-import/view-backup-file-import.js
+++ view-backup-file-import/view-backup-file-import.js
@@ -1,6 +1,17 @@
 class ViewBackupFileImport extends XElement {
     children() { return [XWalletBackupImport] }
     html() {
-        return `<x-wallet-backup-import></x-wallet-backup-import>`;
+        return `
+		<h1>Import Backup File</h1>
+		<h2>Select a backup file to import an account</h2>
+        <x-slides>
+        	<x-wallet-backup-import></x-wallet-backup-import>
+        	<x-pinpad></x-pinpad>
+        </x-slides>
+        `;
     }
 }
+
+// Todo: unlock wallet after import. call api
+// Todo: warn user that importing a different account deletes the current account
+// Todo: [low priority] support multiple accounts at once 
