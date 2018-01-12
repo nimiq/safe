4b2956b7ea7db2b589394d085be50a34af30f513
robinlinus <robinlinus@users.noreply.github.com>
2018-01-11 20:31:47 -0600

    Add view-pin-create
    Former-commit-id: 9dd062123f99cc90f43c1e2a11f2085d0712fd49

---
+++ view-pin-create/view-pin-create.js
@@ -0,0 +1,58 @@
+class ViewPinCreate extends XElement {
+    children() { return [XPinpad] }
+
+    onApiReady(api) {
+        this._api = api;
+        if (!this.$pinpad.unlocking) return;
+        this._encrypt(); // Submit again because api wasn't ready yet
+    }
+
+    onShow() {
+        this.reset();
+    }
+
+    onCreate() {
+        this.addEventListener('x-pin', e => this._submit(e.detail));
+    }
+
+    _reset() {
+        this.$el.removeAttribue('verify');
+        this.$pinpad.reset();
+    }
+
+    _submit(pin) {
+        if (!this._pin) return this._entered();
+        this._verified(pin);
+    }
+
+    _entered(pin) {
+        this._pin = pin;
+        this.$el.setAttribute('verify', true)
+        setTimeout(e => {
+            this.$pinpad.reset();
+        }, 500)
+    }
+
+    _verified(pin) {
+        if (this._pin !== pin) return this.$pinpad.onIncorrectPin();
+        this._encrypt();
+        return true;
+    }
+
+    _encrypt() {
+        if (!this._api) return;
+        this._api.encryptWallet(this._pin)
+            .then(success => this._success())
+    }
+
+    _success() {
+
+    }
+
+    html(){
+        return `
+        <h1>Create a Passcode</h1>
+        <h2>A passcode protects your funds and is used to unlock your Wallet</h2>
+        <x-pinpad></x-pinpad>`
+    }
+}
