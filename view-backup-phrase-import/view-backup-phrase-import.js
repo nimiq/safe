e768347afb97ee9050947bd0d3128bf11829cd8b
robinlinus <robinlinus@users.noreply.github.com>
2018-01-12 19:58:50 -0600

    view-backup-phrase-import: add privacy-agent
    Former-commit-id: fd270a048f15f3c7c4b2c12dd5f471c9b34046b1

--- view-backup-phrase-import/view-backup-phrase-import.js
+++ view-backup-phrase-import/view-backup-phrase-import.js
@@ -1,10 +1,18 @@
 class ViewBackupPhraseImport extends XElement {
-    children() { return [XMnemonicInput] }
+    children() { return [XSlides, XMnemonicInput, XPrivacyAgent] }
+    
+    onCreate() {
+        this.addEventListener('x-surrounding-checked', e => this.$slides.next())
+    }
+
     html() {
         return `
 			<h1>Enter Recovery Phrase</h1>
-			<h2>Type the 24 words of your phrase to recover a wallet</h2>
-			<x-mnemonic-input></x-mnemonic-input>
+			<h2>Type the 24 words of your phrase to recover an account</h2>
+			<x-slides>
+				<x-privacy-agent></x-privacy-agent>
+				<x-mnemonic-input></x-mnemonic-input>
+			</x-slides>
 		`
     }
 }
