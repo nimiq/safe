33ca32ab74c800b53c6b7e000d138d5179a8c81f
robinlinus <robinlinus@users.noreply.github.com>
2018-01-23 14:17:03 -0600

    Add x-toast to x-address

--- x-address/x-address.js
+++ x-address/x-address.js
@@ -1,15 +1,20 @@
 import XElement from '/library/x-element/x-element.js';
+import Clipboard from '/library/clipboard/clipboard.js';
+import XToast from '../x-toast/x-toast.js';
 
 export default class XAddress extends XElement {
+    children() { return [XToast] }
+
     onCreate() {
         this.addEventListener('click', e => this._onCopy())
     }
 
     _onCopy() {
-        
+        Clipboard.copy(this.$el.textContent);
+        this.$toast.show('Address copied to clipboard!')
+    }
+
+    set address(address) {
+        this.$el.textContent = address;
     }
 }
-
-// Todo: copy address on click
-	// Todo: add x-clipboard singleton
-		// Todo: add x-toast
