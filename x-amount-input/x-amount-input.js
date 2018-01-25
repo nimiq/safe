11e0985b82c9808c45a75f5e07a9641d55b81697
robinlinus <robinlinus@users.noreply.github.com>
2018-01-24 18:07:48 -0600

    Refactor amount-input

---
+++ x-amount-input/x-amount-input.js
@@ -0,0 +1,50 @@
+import XInput from '../x-input/x-input.js';
+import NanoApi from '/library/nano-api/nano-api.js';
+export default class XAmountInput extends XInput {
+    html() {
+        return `
+            <form>
+                <x-currency-1>
+                    <input placeholder="00.00" type="number">
+                </x-currency-1>
+                <x-currency-2></x-currency-2>
+            </form>`;
+    }
+
+    onCreate() {
+        super.onCreate();
+        this.$currency2 = this.$('x-currency-2');
+        if (!this._suppressKeyboard) return;
+        this.$input.setAttribute('disabled', '1');
+    }
+
+    _onValueChanged(e) {
+        this._currency2 = this.value;
+    }
+
+    set _currency2(value) {
+        this.$currency2.textContent = NanoApi.formatValueInDollar(value);
+    }
+
+    focus() {
+        if (this._suppressKeyboard) return;
+        super.focus();
+    }
+
+    get _suppressKeyboard() {
+        return window.innerWidth < 420; // Todo: refactor this into a library for mobile-detection
+    }
+
+    _validate() {
+        return this.value > 0;    // Todo: validate if value is correct 
+    }
+
+    set value(value) {
+        super.value = NanoApi.formatValue(value)
+        this._currency2 = value;
+    }
+    
+    get value() { 
+        return Number(this.$input.value);
+    }
+}
--- x-amount/x-amount.js
+++ x-amount/x-amount.js
@@ -1,4 +1,5 @@
 import XElement from '/library/x-element/x-element.js';
+import NanoApi from '/library/nano-api/nano-api.js';
 
 export default class XAmount extends XElement {
     html(){
@@ -7,22 +8,14 @@ export default class XAmount extends XElement {
             <x-currency-2></x-currency-2>`
     }
 
+    onCreate(){
+        this.$currency1 = this.$('x-currency-1')
+        this.$currency2 = this.$('x-currency-2')
+    }
+
     set value(value) {
         value = Number(value);
-        this._currency1 = XAmount.format(value, 3);
-        this._currency2 = XAmount.format(value * 17.1, 2);
-    }
-
-    set _currency1(value) {
-        this.$('x-currency-1').textContent = value;
-    }
-
-    set _currency2(value) {
-        this.$('x-currency-2').textContent = value;
-    }
-
-    static format(number, decimals = 3) {
-        decimals = Math.pow(10, decimals);
-        return Math.round(number * decimals) / decimals;
+        this.$currency1.textContent = NanoApi.formatValue(value, 3) || '';
+        this.$currency2.textContent = NanoApi.formatValueInDollar(value) || '';
     }
 }
