8e29ae59f54e1147d9ef744bc88e5c922ffc07ca
danimoh <github.danimoh@neverbox.com>
2018-01-26 23:36:50 -0600

    x-wallet-backup: fire x-wallet-backup-complete event

--- x-wallet-backup/x-wallet-backup.js
+++ x-wallet-backup/x-wallet-backup.js
@@ -12,11 +12,20 @@ export default class XWalletBackup extends XElement {
         return [XDownloadableImage];
     }
 
+    onCreate() {
+        this.addEventListener('x-image-download', e => this._onImageDownload(e));
+    }
+
     async backup(address, privateKey) {
         const backup = new WalletBackup(address, privateKey);
         const filename = backup.filename();
         const url = await backup.toObjectUrl();
         this.$downloadableImage.src = url;
         this.$downloadableImage.filename = filename;
+    }
+
+    _onImageDownload(e) {
+        e.stopPropagation();
+        this.fire('x-wallet-backup-complete');
     }
 }
