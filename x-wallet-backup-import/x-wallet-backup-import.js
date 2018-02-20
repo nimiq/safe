47243782294746311534219140c7e9364190c23d
sisou <hello@soerenschwert.de>
2018-02-20 10:16:16 -0600

    Wording and spacing

--- x-wallet-backup-import/x-wallet-backup-import.js
+++ x-wallet-backup-import/x-wallet-backup-import.js
@@ -8,7 +8,6 @@ export default class XWalletBackupImport extends XElement {
         return `
             <x-wallet-backup-import-icon></x-wallet-backup-import-icon>
             <p>&nbsp;</p>
-            <p>&nbsp;</p>
             <button>Upload</button>
             <x-wallet-backup-backdrop>Drop wallet file to import</x-wallet-backup-backdrop>
             <input type="file" accept="image/*">`
