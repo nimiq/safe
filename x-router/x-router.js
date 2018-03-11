d48db2b32e62241ed67fa5ec21540e11d2e4d3ae
Sven <sven.buschbeck@gmail.com>
2018-03-10 18:12:04 -0600

    v1 with routing forward and backward

--- x-router/x-router.js
+++ x-router/x-router.js
@@ -6,6 +6,7 @@ export default class XRouter extends XElement {
     //constructor(parent) { super(parent); }
 
     onCreate() {
+        this.reverse = false;
         this.router = new Router({ debug: true, startListening: false });
         //console.log(JSON.stringify(this.router.routes[0].route));
         this.parseRoutes(this.$$('x-route'));
@@ -14,17 +15,10 @@ export default class XRouter extends XElement {
         this.addEventListener('animationend', e => {
             console.log('animationend');
             this._toggleInOut(this.previous, false, false);
+            this._toggleInOut(this.current, false, false);
+            this._setClass(this.current, 'in', true);
+            //this.reverse = false;
         });
-
-        //.add(/\/{path}/, (path) => {
-        //    console.log(this.routes[path]);
-        //})
-        //.add(/\/{path}|{params}/, (path, params) => {
-        //    console.log(this.routes[path], params);
-        //})
-        //.add(/^\/?$/, (path) => {
-        //    console.log("root");
-        //});
     }
 
     parseRoutes(routeElements) {
@@ -35,32 +29,46 @@ export default class XRouter extends XElement {
                 this.routes._root = { path, element }
                 this.router.add(() => {
                     console.log('root route', path);
-                    this.routeTo('_root');
+                    this._show('_root');
                 });
             } else {
                 this.routes[path] = { path, element, regex: new RegExp(path) }
                 this.router.add(new RegExp(path), () => {
-                    this.routeTo(path);
+                    this._show(path);
                     console.log('normal route', path);
                 });
             }
         }
     }
 
-    routeTo(path) {
+    goTo(path) {
+        this.reverse = false;
+        this.router.navigate(path);
+    }
+
+    goBackTo(path) {
+        this.reverse = true;
+        this.router.navigate(path);
+    }
+
+    _show(path) {
         if (path === this.current) return
+        this._setClass(this.current, 'in', false);
         this._toggleInOut(this.current, false)
         this._toggleInOut(path, true);
         [ this.previous, this.current ] = [ this.current, path ];
     }
 
     _toggleInOut(path, setIn, setOut = !setIn) {
-        //if (setOut == undefined) setOut = !setIn;
-        this.setClass(path, 'from-left-in', setIn);
-        this.setClass(path, 'from-right-out', setOut);
+        //this.setClass(path, this.reverse ? 'from-right-in' : 'from-left-in', setIn);
+        //this.setClass(path, this.reverse ? 'from-left-out' : 'from-right-out', setOut);
+        this._setClass(path, 'from-left-in', setIn && this.reverse);
+        this._setClass(path, 'from-right-in', setIn && !this.reverse);
+        this._setClass(path, 'from-right-out', setOut && this.reverse);
+        this._setClass(path, 'from-left-out', setOut && !this.reverse);
     }
 
-    setClass(path, css, on) {
+    _setClass(path, css, on) {
         if (this.routes[path]) this.routes[path].element.classList.toggle(css, on);
     }
 }
