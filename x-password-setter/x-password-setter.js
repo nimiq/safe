04f7ce8a4e79a543f81f20658d5175d912445b49
Don Maxito <maxharz@gmail.com>
2018-03-13 22:30:17 -0600

    password-setter: use new attributes

--- x-password-setter/x-password-setter.js
+++ x-password-setter/x-password-setter.js
@@ -2,15 +2,16 @@ import XPasswordInput from '../x-password-input/x-password-input.js';
 import XPasswordIndicator from '../x-password-indicator/x-password-indicator.js';
 import XElement from '../../libraries/x-element/x-element.js';
 
+
+
 export default class XPasswordSetter extends XElement {
     html() {
-        const buttonLabel = this.attribute('buttonLabel') || 'Confirm';
-        const showIndicator = this.attribute('showIndicator') === 'true';
+        const { buttonLabel, showIndicator } = this.attributes;
 
         return `
             <x-password-input></x-password-input>
-            ${ showIndicator ? `<x-password-indicator></x-password-indicator>` : '' }
-            <button disabled="true">${buttonLabel}</button>
+            ${ showIndicator === true ? `<x-password-indicator></x-password-indicator>` : '' }
+            <button disabled="true">${buttonLabel || 'Confirm'}</button>
         `;
     }
 
