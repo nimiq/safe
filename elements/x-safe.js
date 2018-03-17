5857414b9f2705eac145ae4148c639f30dae5927
Don Maxito <maxharz@gmail.com>
2018-03-16 22:31:00 -0600

    button to start create account flow

--- elements/x-safe.js
+++ elements/x-safe.js
@@ -1,6 +1,7 @@
 import XElement from '/libraries/x-element/x-element.js';
 import XRouter from '/elements/x-router/x-router.js';
 import XSafeStart from './x-safe-start.js';
+import keyguardPromise from '../keyguard.js';
 
 export default class XSafe extends XElement {
 
@@ -15,12 +16,26 @@ export default class XSafe extends XElement {
                 <main x-route="vesting"> Vesting contracts</main>
                 <x-safe-start x-route="/"></x-safe-start>
             </x-router>
-            <x-accounts></x-accounts>
+            <x-accounts>
+                <button>Create new account</button>
+            </x-accounts>
             `
     }
 
     children() {
         return [ XRouter, XSafeStart ];
+    }
+
+    listeners() {
+        return {
+            'click button': () => this._startCreate()
+        }
+    }
+
+    async _startCreate() {
+        const keyguard = await keyguardPromise;
+        const newKey = keyguard.create();
+        console.log(`Got new key ${newKey}`);
     }
 }
 
