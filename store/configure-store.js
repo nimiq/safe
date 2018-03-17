131cc6a778429e6b6e81dbe2cbc4744d28c7fbfa
Don Maxito <maxharz@gmail.com>
2018-03-17 13:51:05 -0600

    remove misleading comment

--- store/configure-store.js
+++ store/configure-store.js
@@ -2,9 +2,6 @@ import { createStore, applyMiddleware, compose, combineReducers } from '/librari
 import { createLogger } from '/libraries/redux-logger/src/index.js';
 import { reducer as accountReducer } from './accounts.js';
 
-// Whenever an action is dispatched, Redux will update each top-level application state property using
-// the reducer with the matching name. It's important that the names match exactly, and that the reducer
-// acts on the corresponding ApplicationState property type.
 const reducers = {
     accounts: accountReducer
 }
