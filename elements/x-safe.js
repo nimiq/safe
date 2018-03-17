a7005036e645adb2a3c31b4224e71a733401ba6b
Don Maxito <maxharz@gmail.com>
2018-03-17 14:09:28 -0600

    button to trigger import from file - missing file upload and qr processing

--- elements/x-safe.js
+++ elements/x-safe.js
@@ -17,7 +17,8 @@ export default class XSafe extends XElement {
                 <x-safe-start x-route="/"></x-safe-start>
             </x-router>
             <x-accounts>
-                <button>Create new account</button>
+                <button id="create">Create new account</button>
+                <button id="import-file">Import account from file</button>
             </x-accounts>
             `
     }
@@ -28,13 +29,20 @@ export default class XSafe extends XElement {
 
     listeners() {
         return {
-            'click button': () => this._startCreate()
+            'click button#create': () => this._startCreate(),
+            'click button#import-file': () => this._startImport()
         }
     }
 
     async _startCreate() {
         const keyguard = await keyguardPromise;
         const newKey = await keyguard.create();
+        console.log(`Got new key ${JSON.stringify(newKey)}`);
+    }
+
+    async _startImport() {
+        const keyguard = await keyguardPromise;
+        const newKey = await keyguard.importFromFile('12e1e112e12e12e12e12e21e');
         console.log(`Got new key ${JSON.stringify(newKey)}`);
     }
 }
