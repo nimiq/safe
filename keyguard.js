5c2e8f0c1dd1a00675de9cd6c3298ce553b34e1a
Don Maxito <maxharz@gmail.com>
2018-03-18 23:24:19 -0600

    pass setState parameter to keyguard-client

--- keyguard.js
+++ keyguard.js
@@ -1,10 +1,12 @@
 import KeyguardClient from '/libraries/keyguard-client/keyguard-client.js';
 import SafePolicy from '/libraries/keyguard/access-control/safe-policy.js';
+import store from './store/store.js';
 import config from './config.js';
 
 class KeyguardSingleton {
     static async getInstance() {
-        this._instance = this._instance || await KeyguardClient.create(config.keyguardSrc, new SafePolicy());
+        this._instance = this._instance ||
+            await KeyguardClient.create(config.keyguardSrc, new SafePolicy(), () => store.getState());
         return this._instance;
     }
 }
