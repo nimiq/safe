7f2977131d28f7a39daa51dbf624e87c399fae68
danimoh <github.danimoh@neverbox.com>
2018-03-19 21:06:55 -0600

    make accounts-list an own x-element

---
+++ x-accounts/x-accounts-list.js
@@ -0,0 +1,53 @@
+import XElement from '/libraries/x-element/x-element.js';
+import { XAccount } from './x-account.js';
+
+export default class XAccountsList extends XElement {
+    html() {
+        return `
+            <x-loading-animation></x-loading-animation>
+            <h2>Loading accounts...</h2>
+        `;
+    }
+
+    onCreate() {
+        this._accountEntries = new Map();
+    }
+
+    _onPropertiesChanged(changes) {
+        if (!changes.accounts) return;
+
+        for (const [ address, account ] of changes.accounts) {
+            const $account = this._accountEntries.get(address);
+            if (account === undefined) {
+                // todo test!
+                $account && $account.destroy();
+                this._accountEntries.delete(address);
+            } else {
+                if ($account) $account.setProperties(account);
+                else this._addAccountEntry(account);
+            }
+        }
+    }
+
+    /**
+     * @param {object} account
+     */
+    _addAccountEntry(account) {
+        const accountEntry = this._createAccountEntry(account);
+        this._accountEntries.set(account.address, accountEntry);
+        if (this.$('x-loading-animation')) {
+            this.$el.textContent = ''; // remove loading animation
+        }
+        this.$el.appendChild(accountEntry.$el);
+    }
+
+    _createAccountEntry(account) {
+        const $account = XAccount.createElement();
+
+        $account.setProperties({
+            ...account
+        });
+
+        return $account;
+    }
+}
--- x-accounts/x-accounts.js
+++ x-accounts/x-accounts.js
@@ -1,21 +1,18 @@
 import XElement from '/libraries/x-element/x-element.js';
 import { XAccount, XAccountModal } from './x-account.js';
+import XAccountsList from './x-accounts-list.js';
 
 export default class XAccounts extends XElement {
     html() {
         return `
             <button create class="small">&plus; Create</button>
             <button import class="small">&#8615; Import</button>
-            <x-accounts-list>
-                <x-loading-animation></x-loading-animation>
-                <h2>Loading accounts...</h2>
-            </x-accounts-list>
+            <x-accounts-list></x-accounts-list>
         `
     }
 
-    onCreate() {
-        this._accounts = new Map();
-        this.$accountsList = this.$('x-accounts-list');
+    children() {
+        return [XAccountsList];
     }
 
     listeners() {
@@ -27,42 +24,8 @@ export default class XAccounts extends XElement {
     }
 
     _onPropertiesChanged(changes) {
-        if (changes.accounts) {
-            if (this.$('x-loading-animation')) {
-                this.$accountsList.textContent = '';
-            }
-
-            for (const [ address, account ] of changes.accounts) {
-                const $account = this._accounts.get(address);
-                if (account === undefined) {
-                    // todo test!
-                    $account && $account.destroy();
-                    this._accounts.delete(address);
-                } else {
-                    if ($account) $account.setProperties(account);
-                    else this.addAccount(account);
-                }
-            }
-        }
-    }
-
-    /**
-     * @param {object} account
-     */
-    addAccount(account) {
-        this._accounts.set(account.address, this._createAccount(account));
-    }
-
-    _createAccount(account) {
-        const $account = XAccount.createElement();
-
-        $account.setProperties({
-            ...account
-        });
-
-        this.$accountsList.appendChild($account.$el);
-
-        return $account;
+        // TODO best practice ?
+        this.$accountsList._onPropertiesChanged(changes);
     }
 
     _onCreateAccount() {
@@ -74,7 +37,7 @@ export default class XAccounts extends XElement {
     }
 
     _onAccountSelected(address) {
-        XAccountModal.instance.setProperties(this._accounts.get(address).properties);
+        XAccountModal.instance.setProperties(this.properties.get(address));
         XAccountModal.show();
     }
 }
