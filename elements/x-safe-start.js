903c052c6b9a53a897a405f86a5a6e8f3f9414b4
Don Maxito <maxharz@gmail.com>
2018-03-22 11:30:32 -0600

    todo

--- elements/x-safe-start.js
+++ elements/x-safe-start.js
@@ -5,21 +5,12 @@ export default class XSafeStart extends XElement {
     html() {
         return `
             <h1>Welcome to Nimiq Safe</h1>
-            <x-switch>
-                <x-variant>
-                    No accounts present? Show introduction and:
-                    <a x-href="import-from-file">import account</a>
-                    <a x-href="new-account">new account</a>
-                 </x-variant>
-                 <x-variant>
-                     otherwise show what?
-                </x-variant>
-            </x-switch>
+            No accounts present? Show introduction and:
+            <a x-href="import-from-file">import account</a>
+            <a x-href="new-account">new account</a>
             `
     }
 }
 
-
-
-// Todo make switch work
+// Todo make this a welcome modal
 // Todo wording, content of this element
