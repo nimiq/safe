5764758d749bace1125986c7a01d845ae172fb2f
sisou <hello@soerenschwert.de>
2018-03-22 21:27:09 -0600

    Store code until needed

--- x-send-transaction/x-send-transaction.js
+++ x-send-transaction/x-send-transaction.js
@@ -70,6 +70,44 @@ export default class XSendTransaction extends XElement {
         form.querySelectorAll('input').forEach(i => formData[i.getAttribute('name')] = i.value);
         return formData;
     }
+
+    static validateAddress(address) {
+        try {
+            this.isUserFriendlyAddress(address);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    // Copied from: https://github.com/nimiq-network/core/blob/master/src/main/generic/consensus/base/account/Address.js
+
+    static isUserFriendlyAddress(str) {
+        str = str.replace(/ /g, '');
+        if (str.substr(0, 2).toUpperCase() !== 'NQ') {
+            throw new Error('Addresses start with NQ', 201);
+        }
+        if (str.length !== 36) {
+            throw new Error('Addresses are 36 chars (ignoring spaces)', 202);
+        }
+        if (this._ibanCheck(str.substr(4) + str.substr(0, 4)) !== 1) {
+            throw new Error('Address Checksum invalid', 203);
+        }
+    }
+
+    static _ibanCheck(str) {
+        const num = str.split('').map((c) => {
+            const code = c.toUpperCase().charCodeAt(0);
+            return code >= 48 && code <= 57 ? c : (code - 55).toString();
+        }).join('');
+        let tmp = '';
+
+        for (let i = 0; i < Math.ceil(num.length / 6); i++) {
+            tmp = (parseInt(tmp + num.substr(i * 6, 6)) % 97).toString();
+        }
+
+        return parseInt(tmp);
+    }
 }
 
 // TODO make fee a slider
