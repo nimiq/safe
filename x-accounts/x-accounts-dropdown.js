0f6e1511477401ad69a220f37f84c78344fb2dbb
danimoh <github.danimoh@neverbox.com>
2018-03-24 17:09:13 -0600

    x-accounts-dropdown: use x-expandable

--- x-accounts/x-accounts-dropdown.js
+++ x-accounts/x-accounts-dropdown.js
@@ -1,4 +1,5 @@
 import XElement from '/libraries/x-element/x-element.js';
+import XExpandable from '../x-expandable/x-expandable.js';
 import XAccount from './x-account.js';
 import XAccountsList from './x-accounts-list.js';
 import MixinRedux from '/elements/mixin-redux/mixin-redux.js';
@@ -7,29 +8,28 @@ export default class XAccountsDropdown extends MixinRedux(XElement) {
 
     html() {
         return `
-            <h3 status-message></h3>
-            <x-account></x-account>
-            <div dropdown-container>
-                <x-accounts-list></x-accounts-list>
-            </div>
+            <x-expandable dropdown disabled>
+                <div expandable-trigger>
+                    <h3 status-message></h3>
+                    <x-account></x-account>
+                </div>
+                <div expandable-content>
+                    <x-accounts-list></x-accounts-list>
+                </div>
+            </x-expandable>
             <input type="hidden">
         `;
     }
 
     children() {
-        return [ XAccount, XAccountsList ];
+        return [ XExpandable, XAccount, XAccountsList ];
     }
 
     onCreate() {
-        this._open = false;
-        this._closeTimeout = null;
         this.$statusMessage = this.$('[status-message]');
-        this.$dropdownContainer = this.$('[dropdown-container]');
         this.$input = this.$('input');
         if (this.attributes.name) this.$input.setAttribute('name', this.attributes.name);
-        this.$el.addEventListener('click', e => e.stopPropagation()); // to avoid body click
         this.$account.addEventListener('x-account-selected', e => e.stopPropagation());
-        this._closeDropdown = this._closeDropdown.bind(this);
         super.onCreate();
     }
 
@@ -43,11 +43,12 @@ export default class XAccountsDropdown extends MixinRedux(XElement) {
 
     _onPropertiesChanged(changes) {
         if (changes.loading === true || changes.hasContent === false) {
+            this.$expandable.disable();
             this._showStatusMessage();
             return;
         }
 
-        this.$el.classList.add('ready');
+        this.$expandable.enable();
 
         if (changes.accounts && !this.selectedAccount) {
             // pre select some arbitrary account
@@ -57,14 +58,13 @@ export default class XAccountsDropdown extends MixinRedux(XElement) {
 
     listeners() {
         return {
-            'x-account-selected x-accounts-list': this._onAccountSelected,
-            'click x-account': this._toggleDropdown
+            'x-account-selected x-accounts-list': this._onAccountSelected
         };
     }
 
     get selectedAccount() {
         const account = this.$account.account;
-        // An XAccount.account always has the hight property,
+        // An XAccount.account always has the height property,
         // thus we check if there are any more than that one
         return Object.keys(account).length > 1 ? account : null;
     }
@@ -82,8 +82,6 @@ export default class XAccountsDropdown extends MixinRedux(XElement) {
     _showStatusMessage() {
         const { loading, hasContent } = this.properties;
         if (hasContent) return;
-        this._closeDropdown();
-        this.$el.classList.remove('ready');
         if (loading) {
             this.$statusMessage.textContent = '';
             const dots = document.createElement('span');
@@ -98,35 +96,6 @@ export default class XAccountsDropdown extends MixinRedux(XElement) {
         const account = this.properties.accounts.get(address);
         this.$account.setProperties(account);
         this.$input.value = account.address;
-        this._closeDropdown();
-    }
-
-    _toggleDropdown() {
-        if (this._open) {
-            this._closeDropdown();
-        } else {
-            this._openDropdown();
-        }
-    }
-
-    _openDropdown() {
-        if (this._open || this.properties.loading || !this.properties.hasContent) return;
-        this._open = true;
-        clearTimeout(this._closeTimeout);
-        document.body.addEventListener('click', this._closeDropdown);
-        this.$dropdownContainer.style.display = 'block';
-        this.$dropdownContainer.offsetWidth; // style update
-        this.$dropdownContainer.style.maxHeight = '200px';
-    }
-
-    _closeDropdown() {
-        if (!this._open) return;
-        this._open = false;
-        document.body.removeEventListener('click', this._closeDropdown);
-        this.$dropdownContainer.style.maxHeight = '0';
-        this._closeTimeout = setTimeout(() => {
-            this.$dropdownContainer.style.display = 'none';
-        }, XAccountsDropdown.ANIMATION_TIME);
+        this.$expandable.collapse();
     }
 }
-XAccountsDropdown.ANIMATION_TIME = 500;
