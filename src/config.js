9805194bc6cff1e519ee35022e22ad1d1929ffa1
Don Maxito <maxharz@gmail.com>
2018-03-30 16:27:35 -0600

    choose config according to host name

--- src/config.js
+++ src/config.js
@@ -1,13 +1,40 @@
-const devConfig = {
-    keyguardSrc: `${location.origin}/libraries/keyguard/src/keyguard.html`,
-    networkSrc: `${location.origin}/libraries/network/src/network.html`,
-    root: `${location.origin}/apps/safe/src`
+function getConfig(host) {
+    switch (host) {
+        case 'https://safe.nimiq.com':
+            return {
+                keyguardSrc: 'https://secure.nimiq.com/keyguard.html',
+                networkSrc: 'https://network.nimiq.com/network.html',
+                root: 'https://safe.nimiq.com',
+                mode: 'live'
+            };
+
+        case 'https://safe.nimiq-testnet.com':
+            return {
+                keyguardSrc: 'https://secure.nimiq-testnet.com/keyguard.html',
+                networkSrc: 'https://network.nimiq-testnet.com/network.html',
+                root: 'https://safe.nimiq-testnet.com',
+                mode: 'test'
+            };
+
+        default:
+            if (location.pathname.includes('/dist')) {
+                return {
+                    keyguardSrc: `${location.origin}/libraries/keyguard/dist/keyguard.html`,
+                    networkSrc: `${location.origin}/libraries/network/dist/network.html`,
+                    root: `${location.origin}/apps/safe/dist`,
+                    mode: 'test'
+                }
+            }
+
+            return {
+                keyguardSrc: `${location.origin}/libraries/keyguard/src/keyguard.html`,
+                networkSrc: `${location.origin}/libraries/network/src/network.html`,
+                root: `${location.origin}/apps/safe/src`,
+                mode: 'test'
+            }
+    }
 }
 
-const liveConfig = {
-    keyguardSrc: 'https://secure.nimiq.com/keyguard.html',
-    networkSrc: 'https://network.nimiq.com/network.html',
-    root: `https://safe.nimiq.com`
-}
+const host = window.location.hostname;
 
-export default devConfig;
+export default getConfig(host);
