8eca0386e7400fe2a1a37a4cb25c6aa752722b73
Don Maxito <maxharz@gmail.com>
2018-03-29 20:49:07 -0600

    build with minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,12 @@ const gulp = require('gulp');
 
 const NimiqBuild = require('../../meta/build-process/nimiq-base-gulpfile.js');
 
-gulp.task('build', () => NimiqBuild.build('safe.js', 'safe.css', 'index.html', [], `${__dirname}/../../`, 'dist'));
+gulp.task('build', () => NimiqBuild.build(
+    'safe.js',
+    'safe.css',
+    'index.html',
+    [],
+    `${__dirname}/../../`,
+    'dist',
+    true
+));
