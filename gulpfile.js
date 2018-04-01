cf0308b9a960dad30cd18f0331dbc16e07b333d7
danimoh <github.danimoh@neverbox.com>
2018-04-01 15:06:17 -0600

    adapt to new base gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
 const gulp = require('gulp');
 
-const argv = require('yargs').argv;
-
 const NimiqBuild = require('../../meta/build-process/nimiq-base-gulpfile.js');
 
-gulp.task('build', () => NimiqBuild.build(
-    'src/safe.js',
-    'src/safe.css',
-    'src/index.html',
-    [],
-    `${__dirname}/../../`,
-    'dist',
-    argv.config,
-    true
-));
+gulp.task('build', () => NimiqBuild.build({
+    jsEntry: 'src/safe.js',
+    cssEntry: 'src/safe.css',
+    htmlEntry: 'src/index.html',
+    rootPath: `${__dirname}/../../`,
+    distPath: 'dist',
+    minify: false
+}));
 
 gulp.task('default', ['build']);
