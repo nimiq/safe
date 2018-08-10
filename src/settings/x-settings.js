de849a882c0208fa3c7523b5771a3d5ce8f84787
sisou <hello@soerenschwert.de>
2018-08-10 13:32:58 +0200

    Store contacts separately from regular persistence
    So that contacts are not deleted when removing cache.

--- src/settings/x-settings.js
+++ src/settings/x-settings.js
@@ -3,6 +3,7 @@ import MixinRedux from '/secure-elements/mixin-redux/mixin-redux.js';
 import XSettingVisualLockModal from './x-setting-visual-lock-modal.js';
 import XSendPreparedTransactionModal from '/elements/x-send-transaction/x-send-prepared-transaction-modal.js';
 import { showAllDecimals } from './settings-redux.js';
+import { Store } from '../store.js';
 
 export default class XSettings extends MixinRedux(XElement) {
     html(){
@@ -29,7 +30,7 @@ export default class XSettings extends MixinRedux(XElement) {
                     Send prepared transaction
                     <small>Send a transaction that was prepared offline.</small>
                 </span>
-                <span class="setting" onclick="localStorage.removeItem('persistedState'); window.skipPersistingState = true; location.reload();">
+                <span class="setting" remove-persistence>
                     Delete cached data
                     <small>This does not delete your accounts. It only deletes your transaction history and balances, which will be loaded again from the network.</small>
                 </span>
@@ -47,7 +48,8 @@ export default class XSettings extends MixinRedux(XElement) {
             'click [show-all-decimals]': this._onClickShowAllDecimals,
             'change [show-all-decimals]>input': this._onClickShowAllDecimals,
             //'click [visual-lock]': this._onClickVisualLock,
-            'click [prepared-tx]': () => XSendPreparedTransactionModal.show()
+            'click [prepared-tx]': () => XSendPreparedTransactionModal.show(),
+            'click [remove-persistence]': this._onClickRemovePersistence,
         }
     }
 
@@ -70,6 +72,17 @@ export default class XSettings extends MixinRedux(XElement) {
         }
 
         XSettingVisualLockModal.show();
+    }
+
+    _onClickRemovePersistence() {
+        Store.persist(); // Persist potential changes in contacts
+
+        // Remove regular persistence
+        localStorage.removeItem('persistedState');
+        // Contacts are not removed on purpose
+
+        window.skipPersistingState = true;
+        location.reload();
     }
 
     static mapStateToProps(state) {
