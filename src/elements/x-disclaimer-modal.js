cc2413105dcfd6b6983c7a0f9304fe577b33e59c
Smitty <smittyvb@gmail.com>
2018-08-17 10:55:48 +0000

    Don't use strings for icon name

--- src/elements/x-disclaimer-modal.js
+++ src/elements/x-disclaimer-modal.js
@@ -6,7 +6,7 @@ export default class XDisclaimerModal extends MixinModal(XElement) {
     html() {
         return `
             <div class="modal-header">
-                <i x-modal-close class="material-icons">${getString('close_modal')}</i>
+                <i x-modal-close class="material-icons">close</i>
                 <h2>Disclaimer</h2>
             </div>
             <div class="modal-body">
