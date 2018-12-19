5da2545ebae8d636da32f59380210492afa20526
Don Maxito <maxharz@gmail.com>
2018-12-19 01:52:24 +0100

    wording

--- x-accounts/x-accounts.js
+++ x-accounts/x-accounts.js
@@ -11,7 +11,7 @@ export default class XAccounts extends MixinRedux(XElement) {
     html() {
         return `
             <x-popup-menu x-icon="add" class="hidden">
-                <button class="add"><i class="material-icons">add</i> Add Address to your Wallet</button>
+                <button class="add"><i class="material-icons">add</i> Add Address to your Account</button>
             </x-popup-menu>
             <x-accounts-list></x-accounts-list>
             <x-account-modal x-route-aside="account"></x-account-modal>
