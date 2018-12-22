5ca1df1831e58aa9cf85a9ac5f78cd8cc6e7b865
sisou <hello@soerenschwert.de>
2018-12-22 20:45:29 +0100

    Integrate XFaucetModal in empty XTransactions list

--- x-transactions/x-no-transactions.js
+++ x-transactions/x-no-transactions.js
@@ -1,11 +1,14 @@
 import XElement from '/libraries/x-element/x-element.js';
 import Config from '/libraries/secure-utils/config/config.js';
+import XFaucetModal from '/apps/safe/src/elements/x-faucet-modal.js';
 
 export default class XNoTransactions extends XElement {
     html() {
         return `
             <h1 class="material-icons">inbox</h1>
-            <span>You have no transactions yet</span>
+            <span>You have no transactions yet.</span>
+            <br>
+            <br><button class="small" faucet>Get your first NIM!</button>
         `
     }
 
@@ -14,5 +17,11 @@ export default class XNoTransactions extends XElement {
             this.$('h1').textContent = 'cloud_off';
             this.$('span').textContent = 'Transactions are not available in offline mode';
         }
+
+        const $faucetButton = this.$('[faucet]');
+        $faucetButton.addEventListener('click', () => {
+            XFaucetModal.show();
+            $faucetButton.disabled = true;
+        });
     }
 }
