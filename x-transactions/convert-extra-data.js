c5e2a3439b1790305dc188853caf50997297cf32
Daniel <github.danimoh@neverbox.com>
2019-01-22 00:54:57 +0100

    x-transaction: avoid import of whole BufferUtils just for equals method

--- x-transactions/convert-extra-data.js
+++ x-transactions/convert-extra-data.js
@@ -1,15 +1,36 @@
 import UTF8Tools from '/libraries/secure-utils/utf8-tools/utf8-tools.js';
-import BufferUtils from '/libraries/nimiq-utils/buffer-utils/buffer-utils.js';
 
 const TransactionTags = {
     SendCashlink: new Uint8Array([0, 130, 128, 146, 135]),
     ReceiveCashlink: new Uint8Array([0, 139, 136, 141, 138]),
 };
 
+function _arrayEquals(a, b) {
+    if ((a.byteLength || a.length) !== (b.byteLength || b.length)) return false;
+    const viewA = _toUint8View(a);
+    const viewB = _toUint8View(b);
+    for (let i = 0; i < viewA.length; i++) {
+        if (viewA[i] !== viewB[i]) return false;
+    }
+    return true;
+}
+
+function _toUint8View(arrayLike) {
+    if (arrayLike instanceof Uint8Array) {
+        return arrayLike;
+    } if (arrayLike instanceof ArrayBuffer) {
+        return new Uint8Array(arrayLike);
+    } else if (arrayLike.buffer instanceof ArrayBuffer) {
+        return new Uint8Array(arrayLike.buffer);
+    } else {
+        return Uint8Array.from(arrayLike);
+    }
+}
+
 export default function convertExtradata(extraData) {
-    if (BufferUtils.equals(extraData, TransactionTags.SendCashlink)) {
+    if (_arrayEquals(extraData, TransactionTags.SendCashlink)) {
         return 'Charging cashlink';
-    } else if (BufferUtils.equals(extraData, TransactionTags.ReceiveCashlink)) {
+    } else if (_arrayEquals(extraData, TransactionTags.ReceiveCashlink)) {
         return 'Redeeming cashlink';
     } else {
         return UTF8Tools.utf8ByteArrayToString(extraData);
