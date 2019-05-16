7b96629bd58f8924a04b2e22888a4e9e85cce032
Don Maxito <maxharz@gmail.com>
2019-05-16 14:35:01 +0200

    label balance with legacy if account is legacy

--- src/elements/x-total-amount.js
+++ src/elements/x-total-amount.js
@@ -2,11 +2,13 @@ import XElement from '/libraries/x-element/x-element.js';
 import MixinRedux from '/secure-elements/mixin-redux/mixin-redux.js';
 import XAmount from '/elements/x-amount/x-amount.js';
 import totalAmount$ from '../selectors/totalAmount$.js';
+import { activeWallet$ } from '../selectors/wallet$.js';
+import { WalletType } from '../wallet-redux.js';
 
 export default class XTotalAmount extends MixinRedux(XElement) {
     html(){
         return `
-            <x-amount white display label="Account balance"></x-amount>
+            <x-amount white display></x-amount>
         `
     }
 
@@ -21,16 +23,19 @@ export default class XTotalAmount extends MixinRedux(XElement) {
 
     static mapStateToProps(state) {
         return {
-            totalAmount: totalAmount$(state)
+            totalAmount: totalAmount$(state),
+            isLegacy: activeWallet$(state).type === WalletType.LEGACY,
         };
     }
 
     _onPropertiesChanged(changes) {
-        const { totalAmount } = changes;
+        const { totalAmount, isLegacy } = changes;
 
         if (totalAmount !== undefined) {
             this.value = totalAmount;
         }
+
+        this.$amount.label = isLegacy ? 'Legacy Account Balance' : 'Account Balance';
     }
 
     set value(value) {
