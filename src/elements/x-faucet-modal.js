4187e3f2603bf4c242ccb875cb1af2a6b10eb3f2
Daniel <github.danimoh@neverbox.com>
2019-05-23 14:01:26 +0200

    x-no-transactions: don't offer free nim if faucet already tapped

--- src/elements/x-faucet-modal.js
+++ src/elements/x-faucet-modal.js
@@ -1,61 +1,10 @@
 import XElement from '../lib/x-element/x-element.js';
 import MixinModal from './mixin-modal/mixin-modal.js';
+import Faucet from '../lib/faucet.js';
 import { default as store } from '../store.js';
 import { activeAddresses$ } from '../selectors/account$.js';
 import XSuccessMark from './x-success-mark/x-success-mark.js';
 import XCaptcha from './x-captcha/x-captcha.js';
-
-class Faucet {
-    static async tap(recipientAddress, captcha) {
-        const responseBody = {
-            address: recipientAddress,
-        };
-        if (captcha) {
-            if (captcha.provider) {
-                responseBody['captcha-provider'] = captcha.provider;
-            }
-            if (captcha.provider === XCaptcha.Providers.RECAPTCHA) {
-                responseBody['g-recaptcha-response'] = captcha.token;
-            } else {
-                responseBody['vaptcha-response'] = captcha.token;
-            }
-        }
-        const response = await fetch(`${Faucet.FAUCET_BACKEND}${Faucet.FAUCET_ENDPOINT_TAP}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(responseBody)
-        }).then(response => response.json());
-        if (!response.success) {
-            const error = new Error('Faucet error');
-            error.error = response.error;
-            error.msg = response.msg;
-            throw error;
-        }
-    }
-
-    static async info() {
-        Faucet._infoPromise = Faucet._infoPromise
-            || fetch(`${Faucet.FAUCET_BACKEND}${Faucet.FAUCET_ENDPOINT_INFO}`).then(response => response.json());
-        return Faucet._infoPromise;
-    }
-
-    static async getDispenseAmount() {
-        const info = await Faucet.info();
-        return info.dispenseAmount; // in NIM
-    }
-
-    static async getFaucetAddress() {
-        const info = await Faucet.info();
-        return info.address;
-    }
-}
-Faucet.FAUCET_BACKEND = window.location.origin.indexOf('nimiq.com') !== -1
-    ? 'https://faucet.nimiq-network.com/' : 'https://faucet.nimiq-testnet.com/';
-Faucet.CAPTCHA_REQUIRED = window.location.origin.indexOf('nimiq.com') !== -1;
-Faucet.FAUCET_ENDPOINT_TAP = 'tapit';
-Faucet.FAUCET_ENDPOINT_INFO = 'info';
 
 export default class XFaucetModal extends MixinModal(XElement) {
     html() {
@@ -107,7 +56,7 @@ export default class XFaucetModal extends MixinModal(XElement) {
         this.$errorMessage = this.$('[error-message]');
         this.$dispenseAmount = this.$('[dispense-amount]');
         if (window.location.origin.indexOf('nimiq.com') !== -1) {
-            this.$el.querySelector('.testnet-nim-hint').remove();
+            this.$('.testnet-nim-hint').remove();
         }
     }
 
--- src/elements/x-transactions/x-no-transactions.js
+++ src/elements/x-transactions/x-no-transactions.js
@@ -1,14 +1,17 @@
 import XElement from '../../lib/x-element/x-element.js';
 import Config from '../../lib/config.js';
 import XFaucetModal from '../../elements/x-faucet-modal.js';
+import Faucet from '../../lib/faucet.js';
 
 export default class XNoTransactions extends XElement {
     html() {
         return `
             <h1 class="material-icons">inbox</h1>
             <span>You have no transactions yet.</span>
-            <br>
-            <br><button class="nq-button light-blue" faucet>Receive free NIM!</button>
+            <div class="faucet-button-container">
+                <br>
+                <button class="nq-button light-blue" faucet>Receive free NIM!</button>
+            </div>
         `
     }
 
@@ -18,10 +21,14 @@ export default class XNoTransactions extends XElement {
             this.$('span').textContent = 'Transactions are not available in offline mode';
         }
 
-        const $faucetButton = this.$('[faucet]');
-        $faucetButton.addEventListener('click', () => {
-            XFaucetModal.show();
-            $faucetButton.disabled = true;
-        });
+        if (Faucet.getLastTapped()) {
+            this.$('.faucet-button-container').remove();
+        } else {
+            const $faucetButton = this.$('[faucet]');
+            $faucetButton.addEventListener('click', () => {
+                XFaucetModal.show();
+                $faucetButton.disabled = true;
+            });
+        }
     }
 }
