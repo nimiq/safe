918d4275f12f333e17e9b1b32f1491c307c2c5c2
Daniel <github.danimoh@neverbox.com>
2019-06-19 21:41:31 +0200

    ReduxProvider: make ReduxProvider a vue class component

--- src/components/ReduxProvider.vue
+++ src/components/ReduxProvider.vue
@@ -1,24 +1,21 @@
 <template>
-  <Provider :mapDispatchToProps="mapDispatchToProps" :mapStateToProps="mapStateToProps" :store="store">
-    <template #default="props">
-      <slot v-bind="props"></slot>
-    </template>
-  </Provider>
+    <Provider :mapDispatchToProps="mapDispatchToProps" :mapStateToProps="mapStateToProps" :store="store">
+        <template v-slot="props">
+            <slot v-bind="props"></slot>
+        </template>
+    </Provider>
 </template>
 
-<script>
+<script lang="ts">
 import store from '../store.js';
 import Provider from 'vuejs-redux';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-export default {
-  props: ['mapDispatchToProps', 'mapStateToProps'],
+@Component({ components: {Provider}})
+export default class ReduxProvider extends Vue {
+    @Prop(Function) public mapStateToProps!: (state: any) => object;
+    @Prop(Function) public mapDispatchToProps!: (dispatch: any) => object;
 
-  components: {
-    Provider,
-  },
-
-  data: () => ({
-    store,
-  }),
-};
+    public store = store;
+}
 </script>
