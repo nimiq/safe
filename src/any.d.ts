3ad7e97bd11bd2c47d6771d0dbb48b4092df8054
Daniel <github.danimoh@neverbox.com>
2019-06-28 17:25:38 +0200

    any.d.ts: Make ts recognize imports from .vue files as vue components

--- src/any.d.ts
+++ src/any.d.ts
@@ -1,2 +1,11 @@
 // enable imports of js files without declarations
+declare module '*.js';
+
+// enable import of vue files
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
+
+// catch everything else that's not typed
 declare module '*';
